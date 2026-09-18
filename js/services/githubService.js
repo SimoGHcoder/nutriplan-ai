f6d732dbe0f1ce060a43d1ef06c59546e339582d
SimoGHcoder <delu.simo@ittmarconiforli.edu.it>
2026-09-18 13:09:12 +0200

    update

--- js/services/githubService.js
+++ js/services/githubService.js
@@ -1,7 +1,7 @@
 // js/services/githubService.js
 
 // Modifica qui il nome del tuo repository se dovesse essere diverso
-const REPO_NAME = "nutripla-ai";
+const REPO_NAME = "nutriplan-ai";
 
 export function getConfigGH() {
   return {
